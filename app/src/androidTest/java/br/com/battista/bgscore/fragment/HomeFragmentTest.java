f2aee8f12d0e785d9300d061e3dcd28a15be0e2e
Rafael Batista <rafaelbs@ciandt.com>
2017-08-17 14:09:53 -0300

    [rabsouza] Add testes para Home

--- app/src/androidTest/java/br/com/battista/bgscore/fragment/HomeFragmentTest.java
+++ app/src/androidTest/java/br/com/battista/bgscore/fragment/HomeFragmentTest.java
@@ -5,8 +5,6 @@ import android.content.Intent;
 import android.support.test.filters.LargeTest;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
-import android.support.v7.widget.Toolbar;
-import android.widget.TextView;
 
 import org.junit.Before;
 import org.junit.Rule;
@@ -18,17 +16,7 @@ import br.com.battista.bgscore.R;
 import br.com.battista.bgscore.activity.HomeActivity;
 import br.com.battista.bgscore.helper.HomeActivityHelper;
 import br.com.battista.bgscore.model.User;
-
-import static android.support.test.espresso.Espresso.onView;
-import static android.support.test.espresso.action.ViewActions.click;
-import static android.support.test.espresso.assertion.ViewAssertions.matches;
-import static android.support.test.espresso.matcher.ViewMatchers.isAssignableFrom;
-import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
-import static android.support.test.espresso.matcher.ViewMatchers.withId;
-import static android.support.test.espresso.matcher.ViewMatchers.withParent;
-import static android.support.test.espresso.matcher.ViewMatchers.withText;
-import static br.com.battista.bgscore.helper.DrawableMatcher.withDrawable;
-import static org.hamcrest.Matchers.allOf;
+import br.com.battista.bgscore.robot.HomeRobot;
 
 @LargeTest
 @RunWith(AndroidJUnit4.class)
@@ -40,11 +28,14 @@ public class HomeFragmentTest {
 
     private User user;
     private Context context;
+    private HomeRobot homeRobot;
 
     @Before
     public void setup() {
         testRule.launchActivity(new Intent());
         context = testRule.getActivity().getApplicationContext();
+
+        homeRobot = new HomeRobot(context);
 
         MainApplication application = (MainApplication)
                 testRule.getActivity().getApplication();
@@ -54,46 +45,27 @@ public class HomeFragmentTest {
         application.clearPreferences();
         application.setUser(user);
 
-        try {
-            onView(withText(R.string.btn_ok))
-                    .perform(click());
-        } catch (Exception e) {
-        }
-
-        onView(withId(R.id.action_home))
-                .perform(click());
-
-        onView(allOf(isAssignableFrom(TextView.class), withParent(isAssignableFrom(Toolbar.class))))
-                .check(matches(withText(R.string.title_home)));
+        homeRobot.closeWelcomeDialog();
+        homeRobot.navigationToHome();
     }
 
     @Test
     public void shouldShowDataUserWhenFirstAccess() {
-        onView(withId(R.id.card_view_home_username))
-                .check(matches(
-                        withText(context.getString(R.string.text_home_username, user.getUsername()))));
-
-        onView(withId(R.id.card_view_home_last_play))
-                .check(matches(
-                        withText(context.getString(R.string.text_home_last_play, "-"))));
-
-        onView(withId(R.id.card_view_home_img)).check(matches(
-                withDrawable(R.drawable.avatar_profile)));
+        homeRobot.checkTextUsername(user.getUsername());
+        homeRobot.checkTextLastPlay("-");
+        homeRobot.checkDrawableUserAvatar(R.drawable.avatar_profile);
     }
 
     @Test
     public void shouldShowDataStaticsWhenFirstAccess() {
-        onView(allOf(withText("00"),
-                withParent(withId(R.id.card_view_score_games))))
-                .check(matches(isDisplayed()));
+        homeRobot.checkScoreValueGames("00");
+        homeRobot.checkScoreValueMatches("00");
+        homeRobot.checkScoreValueTotalTime("00:00");
+    }
 
-        onView(allOf(withText("00"),
-                withParent(withId(R.id.card_view_score_matches))))
-                .check(matches(isDisplayed()));
-
-        onView(allOf(withText("00:00"),
-                withParent(withId(R.id.card_view_score_total_time))))
-                .check(matches(isDisplayed()));
+    @Test
+    public void shouldShowEmptyRankingWhenFirstAccess() {
+        homeRobot.checkEmptyRanking();
     }
 
 }
---
+++ app/src/androidTest/java/br/com/battista/bgscore/robot/HomeRobot.java
@@ -0,0 +1,83 @@
+package br.com.battista.bgscore.robot;
+
+import android.content.Context;
+import android.support.v7.widget.Toolbar;
+import android.widget.TextView;
+
+import br.com.battista.bgscore.R;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isAssignableFrom;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withParent;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static br.com.battista.bgscore.helper.DrawableMatcher.withDrawable;
+import static org.hamcrest.Matchers.allOf;
+
+public class HomeRobot {
+
+    private Context context;
+
+    public HomeRobot(Context context) {
+        this.context = context;
+    }
+
+    public void navigationToHome() {
+        onView(withId(R.id.action_home))
+                .perform(click());
+
+        onView(allOf(isAssignableFrom(TextView.class), withParent(isAssignableFrom(Toolbar.class))))
+                .check(matches(withText(R.string.title_home)));
+    }
+
+    public void closeWelcomeDialog() {
+        try {
+            onView(withText(R.string.btn_ok))
+                    .perform(click());
+        } catch (Exception e) {
+        }
+    }
+
+    public void checkDrawableUserAvatar(int avatar_profile) {
+        onView(withId(R.id.card_view_home_img)).check(matches(
+                withDrawable(avatar_profile)));
+    }
+
+    public void checkTextLastPlay(String lastPlay) {
+        onView(withId(R.id.card_view_home_last_play))
+                .check(matches(
+                        withText(context.getString(R.string.text_home_last_play, lastPlay))));
+    }
+
+    public void checkTextUsername(String username) {
+        onView(withId(R.id.card_view_home_username))
+                .check(matches(
+                        withText(context.getString(R.string.text_home_username, username))));
+    }
+
+    public void checkScoreValueTotalTime(String value) {
+        onView(allOf(withText(value),
+                withParent(withId(R.id.card_view_score_total_time))))
+                .check(matches(isDisplayed()));
+    }
+
+    public void checkScoreValueMatches(String value) {
+        onView(allOf(withText(value),
+                withParent(withId(R.id.card_view_score_matches))))
+                .check(matches(isDisplayed()));
+    }
+
+    public void checkScoreValueGames(String value) {
+        onView(allOf(withText(value),
+                withParent(withId(R.id.card_view_score_games))))
+                .check(matches(isDisplayed()));
+    }
+    public void checkEmptyRanking() {
+        onView(withText(R.string.text_home_stats_empty_view))
+                .check(matches(isDisplayed()));
+    }
+
+}
