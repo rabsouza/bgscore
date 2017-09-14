c3f5189b8dc1a5200d86730ddef05e54c207a0ee
Rafael Batista <rafaelbs@ciandt.com>
2017-09-14 18:21:15 -0300

    [rabsouza] add novos testes para os Fragments/Activity/TestCase

--- app/src/androidTest/java/br/com/battista/bgscore/robot/NewGameRobot.java
+++ app/src/androidTest/java/br/com/battista/bgscore/robot/NewGameRobot.java
@@ -8,6 +8,8 @@ import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
 import static android.support.test.espresso.action.ViewActions.replaceText;
+import static android.support.test.espresso.action.ViewActions.swipeDown;
+import static android.support.test.espresso.action.ViewActions.swipeUp;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static br.com.battista.bgscore.helper.CustomScrollActions.nestedScrollTo;
 
@@ -21,55 +23,56 @@ public class NewGameRobot extends BaseRobot {
 
     public NewGameRobot fillTextName(String name) {
         onView(withId(R.id.card_view_new_game_name))
-                .perform(nestedScrollTo(), replaceText(name), closeSoftKeyboard());
+                .perform(replaceText(name), closeSoftKeyboard());
         return this;
     }
 
     public NewGameRobot fillTextYearPublished(String yearPublished) {
         onView(withId(R.id.card_view_new_game_year_published))
-                .perform(nestedScrollTo(), replaceText(yearPublished), closeSoftKeyboard());
+                .perform(swipeDown(), replaceText(yearPublished), closeSoftKeyboard());
         return this;
     }
 
     public NewGameRobot fillTextAge(String age) {
         onView(withId(R.id.card_view_new_game_age))
-                .perform(nestedScrollTo(), replaceText(age), closeSoftKeyboard());
+                .perform(replaceText(age), closeSoftKeyboard());
         return this;
     }
 
     public NewGameRobot fillTextMinPlayers(String minPlayers) {
         onView(withId(R.id.card_view_new_game_min_players))
-                .perform(nestedScrollTo(), replaceText(minPlayers), closeSoftKeyboard());
+                .perform(replaceText(minPlayers), closeSoftKeyboard());
         return this;
     }
 
     public NewGameRobot fillTextMaxPlayers(String maxPlayers) {
         onView(withId(R.id.card_view_new_game_max_players))
-                .perform(nestedScrollTo(), replaceText(maxPlayers), closeSoftKeyboard());
+                .perform(replaceText(maxPlayers), closeSoftKeyboard());
         return this;
     }
 
     public NewGameRobot fillTextMinPlayTime(String minPlayTime) {
+        nestedScrollTo();
         onView(withId(R.id.card_view_new_game_min_play_time))
-                .perform(nestedScrollTo(), replaceText(minPlayTime), closeSoftKeyboard());
+                .perform(replaceText(minPlayTime), closeSoftKeyboard());
         return this;
     }
 
     public NewGameRobot fillTextMaxPlayTime(String maxPlayTime) {
         onView(withId(R.id.card_view_new_game_max_play_time))
-                .perform(nestedScrollTo(), replaceText(maxPlayTime), closeSoftKeyboard());
+                .perform(replaceText(maxPlayTime), closeSoftKeyboard());
         return this;
     }
 
     public NewGameRobot fillTextGameBadge(String gameBadge) {
         onView(withId(R.id.card_view_new_game_badge_game))
-                .perform(nestedScrollTo(), replaceText(gameBadge), closeSoftKeyboard());
+                .perform(replaceText(gameBadge), closeSoftKeyboard());
         return this;
     }
 
     public GameRobot saveGame() {
         onView(withId(R.id.fab_next_done_game))
-                .perform(nestedScrollTo(), click());
+                .perform(click());
         return new GameRobot(context);
     }
 
